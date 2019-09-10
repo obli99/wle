f761daf326ae0972cd41b18b88b50ff539dbb917
Sayantan <belikesayantan12@gmail.com>
2019-09-10 10:08:25 +0530

    added updater.py
    This script will run the productscrape.py and save the data in data.txt file as json format.

---
+++ updater.py
@@ -0,0 +1,14 @@
+import json
+import productscrape as ps
+
+data = {}
+
+data['amazon'].append({
+    'Item Name': ps.item_name,
+    'Item Price': ps.item_price,
+    'Item Rating': ps.item_rating,
+    'Item Link': ps.item_link,
+})
+
+with open('data.txt', 'w') as json_file:
+    json.dump(data, json_file)
