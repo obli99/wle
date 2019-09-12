dc598335d6789342b36f666e6f53ea6054a70fbe
Sayantan <belikesayantan12@gmail.com>
2019-09-12 21:24:28 +0530

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,11 +1,11 @@
 import json
 
 
-def update(item_name, item_price, item_rating, item_link):
+def update(item_name, integer_item_price, item_rating, item_link):
     data = {'amazon': []}
     data['amazon'].append({
         'Item Name': item_name,
-        'Item Price': item_price,
+        'Item Price': integer_item_price,
         'Item Rating': item_rating,
         'Item Link': item_link,
     })
