35db388cc4df3b5c899471d0f5aaf1ab2489ee7a
Biswajit <patrabiswajit133@gmail.com>
2019-12-01 18:37:34 +0530

    added_variable

--- Extension/script.js
+++ Extension/script.js
@@ -1,1 +1,11 @@
-document.body.style.border = "5px solid red"
+document.body.style.border = "5px solid red";
+//this variable stores the currenturl
+var currentURL;
+chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, 
+function(tabs){
+	getCurrentURL(tabs[0].url);
+});
+function getCurrentURL(tab){
+	currentURL = tab;
+}
+
