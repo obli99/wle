1bcf24eac4ee9d253b5b003ce4348978388cabff
Jyoti Jayadeep <44501265+obli99@users.noreply.github.com>
2020-01-19 19:17:13 +0530

    Update script.js

--- Extension/script.js
+++ Extension/script.js
@@ -1,4 +1,4 @@
-document.body.style.border = "15px solid green";
+document.body.style.border = "15px solid red";
 //this variable stores the currenturl
 var currentURL;
 chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, 
