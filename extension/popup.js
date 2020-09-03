b33d6fd73ac6e43d35fa94a3d085785ab2c124bc
Sayantan Paul <belikesayantan12@gmail.com>
2020-09-04 00:52:08 +0530

    directory renaming and extension styling added (#12)

---
+++ extension/popup.js
@@ -0,0 +1,14 @@
+'use strict'
+
+getCurrentTabUrl();
+
+function getCurrentTabUrl() {
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+    chrome.tabs.query(queryInfo, (tabs) => {
+        var tab = tabs[0];
+        var url = tab.url;
+    });
+}
